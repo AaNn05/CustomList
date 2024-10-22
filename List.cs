1ee63d061e1291f267b1c6cdd30cdbac0f6d2d95
AaNn05 <117485357+AaNn05@users.noreply.github.com>
2024-10-22 16:46:45 +0400

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,68 @@
+﻿namespace CustomList
+{
+    public static class List
+    {
+        public class CustomList<T>
+        {
+            private T[] _items;
+            private int _size;
+
+            public CustomList()
+            {
+                _items = new T[4];
+                _size = 0;
+            }
+
+            public int Count => _size;
+
+            public void Add(T item)
+            {
+                if (_size == _items.Length)
+                {
+                    Resize();
+                }
+                _items[_size++] = item;
+            }
+
+            public T Get(int index)
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                return _items[index];
+            }
+
+            public void Remove(int index)
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                for (int i = index; i < _size - 1; i++)
+                {
+                    _items[i] = _items[i + 1];
+                }
+                _items[--_size] = default;
+            }
+
+            private void Resize()
+            {
+                int newCapacity = _items.Length * 2;
+                T[] newArray = new T[newCapacity];
+                Array.Copy(_items, newArray, _size);
+                _items = newArray;
+            }
+
+            public void PrintList()
+            {
+                for (int i = 0; i < _size; i++)
+                {
+                    Console.Write(_items[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static CustomList.List;
+
+CustomList<int> list = new CustomList<int>();
+
+list.Add(21);
+list.Add(52);
+list.Add(33);
+list.Add(54);
+list.Add(50);
+
+list.PrintList();
+
+Console.WriteLine(list.Get(2));
+
+list.Remove(2); 
+
+list.PrintList();
